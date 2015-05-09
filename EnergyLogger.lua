e8c45b48c849c1723bc80269216991a4531aca08
DND91 <Jonathan.Crusoe@Gmail.com>
2015-05-09 13:32:13 +0200

    dnd.config added eneergy.config

--- EnergyLogger.lua
+++ EnergyLogger.lua
@@ -1,2 +1,16 @@
 -- https://raw.githubusercontent.com/DND91/EnergyLogger/master/EnergyLogger.lua
 
+-- INIT FUNCTIONS
+function loadFile(fileName)
+  local f = fs.open(fileName, "r")
+  if f ~= nil then
+    local data = f.readAll()
+    f.close()
+    return textutils.unserialize(data)
+  end
+end
+
+-- INIT VARIABLES
+
+
+-- PROGRAM BODY
